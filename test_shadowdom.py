f51dc3e8fba2856ddc60055c99ef6b7ae25f790b
l4w45h-linux <viron.dota2@gmail.com>
2025-07-11 10:48:23 +0300

    fix

--- test_shadowdom.py
+++
@@ -1,9 +0,0 @@
-import pytest  # type: ignore
-from pages.shadow_dom_page import ShadowDomPage  # type: ignore
-
-def test_fill_and_verify_shadow_dom(page):
-    shadow_dom_page = ShadowDomPage(page)
-    shadow_dom_page.goto()
-    shadow_dom_page.fill_shadow_input('Hello Shadow DOM!')
-    value = shadow_dom_page.get_shadow_input_value()
-    assert value == 'Hello Shadow DOM!' 

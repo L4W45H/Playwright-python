a9895eac144271b5552ee803c5e88e9b38fabeaa
l4w45h-linux <viron.dota2@gmail.com>
2025-07-14 10:27:43 +0300

    fix async

--- test_api.py
+++ test_api.py
@@ -1,24 +1,24 @@
 import pytest  # type: ignore
-from playwright.sync_api import sync_playwright  # type: ignore
 import requests
+import asyncio
+from playwright.async_api import async_playwright
+import nest_asyncio
 
-def test_get_all_products():
-    with sync_playwright() as p:
-        request_context = p.request.new_context()
-        response = request_context.get('https://dummyjson.com/products')
+async def test_get_all_products():
+    async with async_playwright() as p:
+        request_context = await p.request.new_context()
+        response = await request_context.get('https://dummyjson.com/products')
         assert response.status == 200
-        data = response.json()
+        data = await response.json()
         print(data)
 
-
-def test_add_product():
-    with sync_playwright() as p:
-        request_context = p.request.new_context()
-        response = request_context.post('https://dummyjson.com/products/add', data={"title": "new product"})
+async def test_add_product():
+    async with async_playwright() as p:
+        request_context = await p.request.new_context()
+        response = await request_context.post('https://dummyjson.com/products/add', data={"title": "new product"})
         assert response.status == 201
-        data = response.json()
+        data = await response.json()
         print(data)
-
 
 def test_update_product():
     response = requests.put(
@@ -32,4 +32,7 @@ def test_update_product():
 def test_delete_product():
     response = requests.delete('https://dummyjson.com/products/1')
     data = response.json()
-    print(data) 
+    print(data)
+
+
+nest_asyncio.apply()
